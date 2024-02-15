165db93f65d849902035824878788e57b6e4cc40
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-02-15 12:22:45 +0100

    Bug 2 -add comments

--- Billed-app-FR-Front/src/containers/Login.js
+++ Billed-app-FR-Front/src/containers/Login.js
@@ -41,6 +41,7 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
+      //== Fixing BUG 2: replace 2 input[data-testid...] with appropriate content ( "admin-..."" replace "employee-..."")
       email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
